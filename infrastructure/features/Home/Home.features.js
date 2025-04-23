af229718b4e8bb33885d14d1ac6c3f0301276ca6
Nahuel Gigena <nahuel.gigena@hotmail.com>
2025-04-23 01:57:50 -0300

    ADD: Skeleton

--- infrastructure/features/Home/Home.features.js
+++ infrastructure/features/Home/Home.features.js
@@ -1,56 +1,29 @@
-const zapatillas = [
-  {
-    nombre: "Air Jordan 1 High OG",
-    precio: "$45.000",
-    categoty: "men",
-    imagen:
-      "https://nikearprod.vtexassets.com/arquivos/ids/1427071-1400-1400?v=638786891842500000&width=1400&height=1400&aspect=true",
-  },
-  {
-    nombre: "Nike Tiempo Legend 10 Elite LX",
-    precio: "$50.000",
-    categoty: "men",
-    imagen:
-      "https://nikearprod.vtexassets.com/arquivos/ids/1369216-1400-1400?v=638739475649800000&width=1400&height=1400&aspect=true",
-  },
-  {
-    nombre: "Sabrina 2 Stronger Than Gold",
-    categoty: "women",
-    precio: "$38.000",
-    imagen:
-      "https://nikearprod.vtexassets.com/arquivos/ids/1393637-1400-1400?v=638785022807900000&width=1400&height=1400&aspect=true",
-  },
-  {
-    nombre: "Air Jordan 1 Retro High OG",
-    precio: "$38.000",
-    categoty: "women",
-    imagen:
-      "https://nikearprod.vtexassets.com/arquivos/ids/1427277-1400-1400?v=638786894595030000&width=1400&height=1400&aspect=true",
-  },
-  {
-    nombre: "Nike Revolution 7",
-    precio: "$38.000",
-    categoty: "women",
-    imagen:
-      "https://nikearprod.vtexassets.com/arquivos/ids/1328251-1200-1200?width=1200&height=1200&aspect=true",
-  },
-  {
-    nombre: "Air Jordan 1 Low",
-    precio: "$198.000",
-    categoty: "men",
-    imagen:
-      "https://nikearprod.vtexassets.com/arquivos/ids/844396-1200-1200?width=1200&height=1200&aspect=true",
-  },
-];
+import { ProductsService } from "../../../domain/services/products.service.js";
 
+const service = new ProductsService();
 const contenedor = document.getElementById("zapatillas-list");
+const loading = document.getElementById("loading");
 
-zapatillas.forEach((zapa) => {
-  contenedor.innerHTML += `
-      <div class="sneakerCard">
+async function mostrarZapatillas() {
+  loading.style.display = "grid";
+  contenedor.style.display = "none";
+
+  const zapatillas = await service.getAllProducts();
+
+  zapatillas.forEach((zapa) => {
+    contenedor.innerHTML += `
+      <div class="flex flex-col gap-2 sneakerCard">
         <img src="${zapa.imagen}" alt="${zapa.nombre}" />
-        <h3>${zapa.nombre}</h3>
-        <span>${zapa.precio}</span>
+        <div class="flex flex-col content">        
+          <span class="font-bold sneakerName">${zapa.nombre}</span>
+          <span>${zapa.precio}</span>
+        </div>
       </div>
     `;
-});
+  });
+
+  loading.style.display = "none";
+  contenedor.style.display = "grid";
+}
+
+mostrarZapatillas();
